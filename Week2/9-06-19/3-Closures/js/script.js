2b747fdcbe1b242f4938d9df4efc6087bbb3e0a7
le-christine <christine.le244@gmail.com>
2019-09-07 09:05:25 -0400

    another closure example

--- Week2/9-06-19/3-Closures/js/script.js
+++ Week2/9-06-19/3-Closures/js/script.js
@@ -1,17 +1,21 @@
-function buildName (name) {
-  let greeting = `Hello ${name}!`;
-  // this is a closure. It is  a function that has access to the greeting
-  // if sayName() wasn't nested it would run once and be sent to the garbage
-  // Closures keep things around in memory
-  let sayName = function () {
-    let welcome = greeting + " Welcome!";
-    console.log(greeting);
-  };
+function buildContor(i) {
+    let contor = 1;
+    // this is a closure
+    // these two functions work as a unit
+    let displayContor = function () {
+      console.log(contor++);
+      contor++;
+    };
 
-  return sayName();
+    return displayContor;
 }
 
-var sayName = buildName("John");
+let myContor = buildContor(1);
+myContor();
+myContor();
+myContor();
 
-
-  // scope and closure are similar, but scope is only applied to if (x = 100) { a scope not a closure} else { another scope }
+let otherContor = buildContor(10);
+otherContor();
+otherContor();
+otherContor();
