3ee7b00c44f3869a34a964b1f20a513953ebf40c
le-christine <christine.le244@gmail.com>
2019-09-16 13:57:42 -0400

    boolean or logical operators

--- Week4/1-Monday/practice/src/Sample.java
+++ Week4/1-Monday/practice/src/Sample.java
@@ -1,29 +1,12 @@
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        int age = 32;
-        boolean isStudent = false;
-        double grade = 65.2;
-        String name = "John";
-
-//        if (isStudent == true) {
-//            System.out.println("Student status: True");
-//        } else {
-//            System.out.println("Student status: False");
-//        }
-
-//        int i = 9;
-
-//        if (i < 0) {
-//            System.out.println("Hi");
-//        } else {
-//            System.out.println("Goodbye");
-//        }
-
-        // Ternary statements
-//        String  allowed = (i < 0) ? "Hi" : "Goodbye";
-//        System.out.println(allowed);
-
-        
+        int age = 18;
+        double gpa = 3.2;
+        boolean isTrue = false;
+        if (!isTrue) {
+            System.out.println("Good student");
+        } else {
+            System.out.println("Wait list");
+        }
     }
 }
