d65527a75c857c76c349e819483602102de86372
le-christine <christine.le244@gmail.com>
2019-09-16 14:27:56 -0400

    Completed FizzBuzz game - independent practice

--- Week4/1-Monday/practice/src/Sample.java
+++ Week4/1-Monday/practice/src/Sample.java
@@ -1,19 +1,24 @@
 public class Sample {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
+        /*
+         Fizz buzz is a game about division.
+         Create a program that will iterate through numbers 1 to 101, and log each number in the console.
 
-        int i = 0;
-        while (i<10) {
-            System.out.println(i);
-            i++;
-        }
+        If the number is divisible by three, the word fizz should be logged instead of logging the number itself.
+        If the number is divisible by five, the word buzz should be logged.
+        If the number is divisible by both three and five, the word fizzbuzz should be logged.
+         */
 
-        int z = 0;
-        do {
-            System.out.println(z);
-            z++;
-        } while (z<10);
+        for (int i = 1; i < 102; i++ ) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0 && i % 3 != 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
