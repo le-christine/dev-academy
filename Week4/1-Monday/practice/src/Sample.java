a1e9c8db66adac77ff5d2d1b94359416c58897ba
le-christine <christine.le244@gmail.com>
2019-09-16 14:16:25 -0400

    equality among Strings

--- Week4/1-Monday/practice/src/Sample.java
+++ Week4/1-Monday/practice/src/Sample.java
@@ -7,9 +7,12 @@ public class Sample {
         String bl = "bl";
         String ue = "ue";
         System.out.println(bl+ ue);
-        boolean withSign = (bl + ue == blue);
+        boolean withSign = (bl + ue == blue); //location memory differs, so false
         // boolean withSign = (bl + ue).equals(blue);
         System.out.println(withSign);
 
+//        int x; // declaration
+//        x = 100; // initalization
+//        System.out.println(x);
     }
 }
