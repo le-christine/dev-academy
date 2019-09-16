079eb714888079c5fd91439f6eaa9ff87b301ac9
le-christine <christine.le244@gmail.com>
2019-09-16 16:24:33 -0400

    completed independent practice

--- Week4/1-Monday/practice/src/Sample.java
+++ Week4/1-Monday/practice/src/Sample.java
@@ -1,36 +1,75 @@
 import java.util.Scanner;
+import java.lang.Character;
 
 public class Sample {
     public static void main(String[] args) {
+        System.out.println(divide152By(3));
+        System.out.println(divide152By(43));
+        System.out.println(divide152By(8));
 
-        // how to get user input
-        Scanner input = new Scanner(System.in); // creates a new Scanner object from the java.util library;
-        int input1 = input.nextInt();
-        int input2 = input.nextInt();
-        int answer = Calc(input1, input2,'a');
-        boolean passOrFail = getScores(3.9, 'a');
-        System.out.println(answer);
-        System.out.println(passOrFail);
+        System.out.println(thirdAndFirst("billygoat","LION"));
+        System.out.println(thirdAndFirst("drEAMcaTCher","statue"));
+        System.out.println(thirdAndFirst("Times","thyme"));
+
+        System.out.println(transmogrifier(5, 4, 3));
+        System.out.println(transmogrifier(13, 12, 5));
+        System.out.println(transmogrifier(42, 13, 7));
+
+        System.out.println(reverseString("black cat")); // => "tac kcalb"
+        System.out.println(reverseString("the cow jumped over the moon")); // => "noom eht revo depmuj woc eht"
+        System.out.println(reverseString("I can ride my bike with no handlebars")); // => "srabeldnah on htiw ekib ym edir nac I"
     }
 
-    public static int Calc(int a, int b, char choice) {
-        int result = 0;
-        switch (choice) {
-            case 'a':
-                // cannot return a + b from here because the whole method has to return
-                result = a+b;
-                break;
-            case 'm' :
-                result = a*b;
-                break;
-            default:
-                System.out.println("Enter the correct option");
+
+    /*
+    Write a method called divide152By().
+    It should accept a number as an argument and divide 152 by the given number.
+    For example, the divide152By result of 2 — 152/2 — is 76. Your method should return the result.
+     */
+
+        public static int divide152By ( int number){
+            return 152 / number;
         }
-        return result;
+
+    /*
+    Write a method called thirdAndFirst().
+    This method accepts two strings and checks if the third letter of the first string is the same as the first letter of the second string.
+    It should be case insensitive, meaning thirdAndFirst("Apple","Peon") should return true.
+    */
+
+        public static boolean thirdAndFirst(String firstStr, String secondStr) {
+            char third = Character.toUpperCase(firstStr.charAt(2));
+            char first = Character.toUpperCase(secondStr.charAt(0));
+            return third == first;
+        }
+
+    /*
+    Write a method called transmogrifier().
+    This method should accept three arguments, which you can assume will be numbers.
+    Your method should return the "transmogrified" result, which is the product of the first two numbers raised to the power of the third number.
+    For example, the transmogrified result of 5, 3, and 2 — (5 times 3) to the power of 2 — is 225.
+     */
+
+    public static int transmogrifier(int a, int b, int c) {
+        int transmogrified = (a*b)^c;
+        return transmogrified;
     }
 
-    public static boolean getScores(double gpa, char letterGrade) {
-        return gpa > 3.1 && letterGrade == 'A' || letterGrade == 'a';
-    }
+    /*
+    Write a method called reverseString().
+    This method should take one argument, a String.
+    The method should return a String with the order of the words reversed.
+    Don't worry about punctuation.
+     */
 
+    public static String reverseString(String toBeReversed) {
+        String reverse = "";
+        for (int i = toBeReversed.length()-1; i >= 0; i-- ) {
+            reverse += toBeReversed.charAt(i);
+        }
+        return reverse;
+    }
 }
+
+
+
