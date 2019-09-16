34464c7726cc4d93a6027676e155155c8e76a82c
le-christine <christine.le244@gmail.com>
2019-09-16 14:20:15 -0400

    for loop

--- Week4/1-Monday/practice/src/Sample.java
+++ Week4/1-Monday/practice/src/Sample.java
@@ -1,16 +1,7 @@
 public class Sample {
     public static void main(String[] args) {
-        String food = "apple";
-
-        switch (food) {
-            case "apple":
-                System.out.println("Apple");
-                break;
-            case "pear":
-                System.out.println("Pear");
-                break;
-            default:
-                System.out.println("Cannot find the food");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
         }
     }
 }
