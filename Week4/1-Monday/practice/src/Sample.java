acd4d48af7601540a3bef34e84b39b26b0865551
le-christine <christine.le244@gmail.com>
2019-09-16 13:44:16 -0400

    added java control flow and loops lesson

---
+++ Week4/1-Monday/practice/src/Sample.java
@@ -0,0 +1,15 @@
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int age = 32;
+        boolean isStudent = true;
+        double grade = 65.2;
+        String name = "John";
+
+        if (isStudent == true) {
+            System.out.println("Student status: True");
+        } else {
+            System.out.println("Student status: False");
+        }
+    }
+}
