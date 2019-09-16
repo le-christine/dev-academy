d8e183d71a3aeec7a4427b08e311ade84efd21dd
le-christine <christine.le244@gmail.com>
2019-09-16 14:23:07 -0400

    added while and do while loops

--- Week4/1-Monday/practice/src/Sample.java
+++ Week4/1-Monday/practice/src/Sample.java
@@ -3,5 +3,17 @@ public class Sample {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
+
+        int i = 0;
+        while (i<10) {
+            System.out.println(i);
+            i++;
+        }
+
+        int z = 0;
+        do {
+            System.out.println(z);
+            z++;
+        } while (z<10);
     }
 }
