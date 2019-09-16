c06d1f6cc66ed631161484602f5834e10be1540d
le-christine <christine.le244@gmail.com>
2019-09-16 13:51:17 -0400

    if/else and ternary statement

--- Week4/1-Monday/practice/src/Sample.java
+++ Week4/1-Monday/practice/src/Sample.java
@@ -2,14 +2,28 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello World");
         int age = 32;
-        boolean isStudent = true;
+        boolean isStudent = false;
         double grade = 65.2;
         String name = "John";
 
-        if (isStudent == true) {
-            System.out.println("Student status: True");
-        } else {
-            System.out.println("Student status: False");
-        }
+//        if (isStudent == true) {
+//            System.out.println("Student status: True");
+//        } else {
+//            System.out.println("Student status: False");
+//        }
+
+//        int i = 9;
+
+//        if (i < 0) {
+//            System.out.println("Hi");
+//        } else {
+//            System.out.println("Goodbye");
+//        }
+
+        // Ternary statements
+//        String  allowed = (i < 0) ? "Hi" : "Goodbye";
+//        System.out.println(allowed);
+
+        
     }
 }
