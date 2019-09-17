8b46bffb19bd0ba93ca38fb85fdc1b285e518a66
le-christine <christine.le244@gmail.com>
2019-09-17 14:11:10 -0400

    first debug example

---
+++ Week4/2-Tuesday/4-Debugging-Lesson/src/DebugExample.java
@@ -0,0 +1,13 @@
+public class DebugExample {
+    public static void main(String args[]) {
+        printSomething();
+    }
+
+    public static void printSomething(String s) {
+        System.out.println(s);
+        System.out.println(s.length());
+    }
+
+
+
+}
