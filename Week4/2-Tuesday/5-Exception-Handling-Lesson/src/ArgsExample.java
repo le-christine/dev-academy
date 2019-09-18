3d5f659439c88c01d0e2a9583e25b30a6111c44a
le-christine <christine.le244@gmail.com>
2019-09-18 11:17:04 -0400

    Renamed directories to match previous weeks

--- Week4/2-Tuesday/5-Exception-Handling-Lesson/src/ArgsExample.java
+++
@@ -1,8 +0,0 @@
-public class ArgsExample {
-    // The program arguments have been configured
-    public static void main(String args[]) {
-        for (int x = 0; x<args.length; x++) {
-            System.out.println(args[x] + " " + args[x].getClass());
-        }
-    }
-}
