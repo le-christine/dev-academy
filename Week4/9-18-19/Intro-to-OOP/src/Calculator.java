9e6a587945c8c9a15bb550319dad4f83a1486041
le-christine <christine.le244@gmail.com>
2019-09-18 15:01:37 -0400

    lesson continued

--- Week4/9-18-19/Intro-to-OOP/src/Calculator.java
+++ Week4/9-18-19/Intro-to-OOP/src/Calculator.java
@@ -5,14 +5,19 @@ public class Calculator {
     double first, double second
     return the sum of the first plus second
      */
-    public static double Sum(double num1, double num2) {
+    public double Sum(double num1, double num2) {
         return num1 + num2;
     }
 
+    public double multiply(double num1, double num2) {
+        return num1*num2;
+    }
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println(calc.Sum(10.50,5.75));
         System.out.println(calc.Sum(5,6));
         System.out.println(calc.Sum(4.56,5.46));
+        System.out.println(calc.multiply(200,12));
+        System.out.println(calc.multiply(20,30));
     }
 }
