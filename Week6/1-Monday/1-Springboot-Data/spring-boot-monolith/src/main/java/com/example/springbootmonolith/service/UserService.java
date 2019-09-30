585882e9e8df0c1fe1b857dd8065ad9b2f576eb9
Christine Le <christine.le244@gmail.com>
2019-09-30 16:37:21 -0400

    complete intro to spring boot classwork

---
+++ Week6/1-Monday/1-Springboot-Data/spring-boot-monolith/src/main/java/com/example/springbootmonolith/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.springbootmonolith.service;
+
+import com.example.springbootmonolith.models.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+    public User getUser(String username);
+
+    public Iterable<User> listUsers();
+
+    public String createUser(User newUser);
+
+    public HttpStatus deleteById(Long userId);
+
+    public User addCourse(String username, int courseId);
+
+    public String login(User user);
+
+
+
+}
