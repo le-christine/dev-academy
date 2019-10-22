ca3a30100e9b5cc12f176b7449e91983887c62b8
Christine Le <christine.le244@gmail.com>
2019-10-22 12:50:32 -0400

    refactor so App is the parent and mood is a function

--- Week9/10-22-19/React-Components-Lab/state-practice/src/App.js
+++ Week9/10-22-19/React-Components-Lab/state-practice/src/App.js
@@ -5,9 +5,52 @@ import './App.css';
 import Mood from './Mood';
 class App extends Component {
 
+  // What will happen when the component is first created
+  constructor(props) {
+    // Make call to parent class (Component) constructor
+    super(props);
+
+    this.state = {
+      moodPoints: 1, // iniitialize this.state.moodPoints
+      mood: "happy"
+     }
+
+     // bind callbacks to maintain context
+     this.changeMood = this.changeMood.bind(this);
+     this.increaseMood = this.increaseMood.bind(this);
+     this.decreaseMood = this.decreaseMood.bind(this);
+  }
+
+  changeMood(e) {
+    const newMoodPoints = prompt("How are you feeling (1-10)?");
+    const newMood = parseInt(newMoodPoints,10) <= 10 ? parseInt(newMoodPoints,10) : 1;
+    this.setState({
+        moodPoints: newMood
+      })
+  }
+
+  increaseMood(e) {
+    const newMood = this.state.moodPoints < 10 ? (this.state.moodPoints + 1) : 1;
+    this.setState({
+        moodPoints: newMood
+      })
+    }
+
+  decreaseMood(e) {
+    const newMood = this.state.moodPoints < 10 && this.state.moodPoints > 1 ? (this.state.moodPoints - 1) : 1;
+    this.setState({
+        moodPoints: newMood
+      })
+  }
+
   render () {
     return (
-        <Mood mood={"happy"} />
+        <Mood
+          decreaseMood = {this.decreaseMood}
+          increaseMood = {this.increaseMood}
+          changeMood = {this.changeMood}
+          mood={this.state.mood}
+          moodPoints ={this.state.moodPoints} />
     );
   }
 }
--- Week9/10-22-19/React-Components-Lab/state-practice/src/Mood.js
+++ Week9/10-22-19/React-Components-Lab/state-practice/src/Mood.js
@@ -1,56 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Mood extends Component {
-  // What will happen when the component is first created
-  constructor(props) {
-    // Make call to parent class (Component) constructor
-    super(props);
+const Mood = props => {
 
-    this.state = {
-      moodPoints: 1 // iniitialize this.state.moodPoints
-     }
 
-     // bind callbacks to maintain context
-     this.changeMood = this.changeMood.bind(this);
-     this.increaseMood = this.increaseMood.bind(this);
-     this.decreaseMood = this.decreaseMood.bind(this);
-  }
-
-  changeMood(e) {
-    const newMoodPoints = prompt("How are you feeling (1-10)?");
-    const newMood = parseInt(newMoodPoints,10) <= 10 ? parseInt(newMoodPoints,10) : 1;
-    this.setState({
-        moodPoints: newMood
-      })
-  }
-
-  increaseMood(e) {
-    const newMood = this.state.moodPoints < 10 ? (this.state.moodPoints + 1) : 1;
-    this.setState({
-        moodPoints: newMood
-      })
-    }
-
-  decreaseMood(e) {
-    const newMood = this.state.moodPoints < 10 && this.state.moodPoints > 1 ? (this.state.moodPoints - 1) : 1;
-    this.setState({
-        moodPoints: newMood
-      })
-  }
-
-  render () {
     return (
       <div>
         <p>On a scale of 1-10</p>
-        <p>You are this {this.props.mood}:
+        <p>You are this {props.mood}:
         <span className="moodColor"
-        style={{color: this.state.moodPoints > 5 ? "orange" : "blue"}}> {this.state.moodPoints}</span></p>
-        <button onClick = {this.changeMood}> Cheer up!</button>
-        <button onClick={this.increaseMood}>Increase {this.props.mood}</button>
-        <button onClick={this.decreaseMood}>Decrease {this.props.mood}</button>
+        style={{color: props.moodPoints > 5 ? "orange" : "blue"}}> {props.moodPoints}</span></p>
+        <button onClick = {props.changeMood}> Cheer up!</button>
+        <button onClick={props.increaseMood}>Increase {props.mood}</button>
+        <button onClick={props.decreaseMood}>Decrease {props.mood}</button>
       </div>
     )
   }
-}
+
 
 export default Mood;
