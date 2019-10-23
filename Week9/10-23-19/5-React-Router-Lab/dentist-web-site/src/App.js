e48521a443472552c7cacaa99353331f76545c87
Christine Le <christine.le244@gmail.com>
2019-10-23 15:20:17 -0400

    made contact and procedures pages

--- Week9/10-23-19/5-React-Router-Lab/dentist-web-site/src/App.js
+++ Week9/10-23-19/5-React-Router-Lab/dentist-web-site/src/App.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 
 // Custom components
 import Home from './Home'
+import Procedures from './Procedures';
+import Contact from './Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
 
-        {/* Another way to call components
+        {/* Two ways to call components
+            <Home />
             <Home></Home>
         */}
+
+        <Router>
+        <div exact path="/" component={Home} />
 
       </div>
     );
