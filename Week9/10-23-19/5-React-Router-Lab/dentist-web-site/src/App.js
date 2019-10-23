9208fe29e9c545cef7de324b82bf597ae1174c45
Christine Le <christine.le244@gmail.com>
2019-10-23 15:16:37 -0400

    created a homepage that App.js calls

--- Week9/10-23-19/5-React-Router-Lab/dentist-web-site/src/App.js
+++ Week9/10-23-19/5-React-Router-Lab/dentist-web-site/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// Custom components
+import Home from './Home'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Home />
+
+        {/* Another way to call components
+            <Home></Home>
+        */}
 
       </div>
     );
---
+++ Week9/10-23-19/5-React-Router-Lab/dentist-web-site/src/Procedures.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Procedures extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Procedures;
